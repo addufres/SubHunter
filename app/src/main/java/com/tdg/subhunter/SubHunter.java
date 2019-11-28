2bcc6bfd818b69e2b587dfe721015d552e6115fe
AndrewD <adufresplatinum@gmail.com>
2019-11-27 22:42:54 -0500

    added variables to class... really inchin this one along..

--- app/src/main/java/com/tdg/subhunter/SubHunter.java
+++ app/src/main/java/com/tdg/subhunter/SubHunter.java
@@ -15,64 +15,121 @@ import java.util.Random;
 
 public class SubHunter extends Activity {
 
-    /*
-    Android runs this code just before
-    the player sees the app.
-    This makes it a good place to add
-    the code for the one-time setup phase.
+    /**
+     *
+     * Variables to be used throughout the SubHunter class
+     */
+    private int numXPixels;
+    private int numYPixels;
+    private int blockSize;
+    private int gridWidth = 40;
+    private int gridHeight;
+    private float xPixelTouched = -100;
+    private float yPixelTouched = -100;
+    private int subXPos;
+    private int subYPos;
+    private boolean hit = false;
+    private int shotsTaken;
+    private int distanceFromSub;
+    private boolean debugging = true;
+
+
+
+
+    /**
+     *  Android runs this code just before
+     *  the player sees the app.
+     *  This makes it a good place to add
+     *  the code for the one-time setup phase.
     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Get the current device's screen resolution 
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+
+        // Initialize size based variables
+        numXPixels = size.x;
+        numYPixels = size.y;
+        blockSize = numXPixels / gridWidth;
+        gridHeight = numYPixels / blockSize;
+
+        // Start the game
+        Log.d("Debugging", "In onCreate");
+        newGame();
+        draw();
     }
 
-    /*
-        This code will execute when a new
-        game needs to be started. It will
-        happen when the app is first started
-        and after the player wins a game.
+    /**
+     *   This code will execute when a new
+     *   game needs to be started. It will
+     *   happen when the app is first started
+     *   and after the player wins a game.
      */
-    void newGame() {
-
+    private void newGame() {
+        Log.d("Debugging", "In newGame");
     }
 
-    /*
-        Here we will do all the drawing.
-        The grid lines, the HUD and
-        the touch indicator
+    /**
+     *   Here we will do all the drawing.
+     *   The grid lines, the HUD and
+     *   the touch indicator
      */
-    void draw() {
-
+    private void draw() {
+        Log.d("Debugging", "In draw");
+        printDebuggingText();
     }
 
-    /*
-        This part of the code will
-        handle detecting that the player
-        has tapped the screen
+    /**
+     *   This part of the code will
+     *   handle detecting that the player
+     *   has tapped the screen
      */
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        return false;
+        Log.d("Debugging", "In onTouchEvent");
+        takeShot();
+        return true;
     }
 
-    /*
-        The code here will execute when
-        the player taps the screen. It will
-        calculate the distance from the sub'
-        and decide a hit or miss
+    /**
+     *   The code here will execute when
+     *   the player taps the screen. It will
+     *   calculate the distance from the sub'
+     *   and decide a hit or miss
      */
-    void takeShot() {
-
+    private void takeShot() {
+        Log.d("Debugging", "In takeShot");
+        draw();
     }
 
 
-    // This code says "BOOM!"
-    void boom() {
+    /**
+     * This code says "BOOM!"
+     */
+    private void boom() {
 
     }
 
-    // This code prints the debugging text
-    void printDebuggingText() {
-
+    /**
+     * This code prints the debugging text
+     */
+    private void printDebuggingText() {
+        Log.d("numXPixels: "," " + numXPixels);
+        Log.d("numYPixels: "," " + numYPixels);
+        Log.d("blockSize: "," " + blockSize);
+        Log.d("gridWidth: "," " + gridWidth);
+        Log.d("gridHeight" ," " + gridHeight);
+        Log.d("xPixelTouched: "," " + xPixelTouched);
+        Log.d("yPixelTouched: "," " + yPixelTouched);
+        Log.d("subXPos: "," " + subXPos);
+        Log.d("subYPos: "," " + subYPos);
+        Log.d("hit: "," " + hit);
+        Log.d("shotsTaken: "," " + shotsTaken);
+        Log.d("debugging: "," " + debugging);
+        Log.d("distanceFromSub: "," " + distanceFromSub);
     }
 }
